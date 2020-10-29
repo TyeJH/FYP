c3b5111283ad82b098e0a4846d922588de5df550
USER <USER@LAPTOP-RNH6V26M>
2020-10-30 00:11:32 +0800

    Wait for combine and do some tidy up

--- UI/HomePage.php
+++ UI/HomePage.php
@@ -32,7 +32,7 @@ session_start();
                                     <a class="nav-link" href="../UI/HomePageEvents.php">Events</a>
                                 </li>
                                 <li class="nav-item active">
-                                    <a class="nav-link" href="../UI/Announcement.php">Announcement</a>
+                                    <a class="nav-link" href="../UI/CreateAnnouncement.php">Announcement</a>
                                 </li>
                                 <li class="nav-item active">
                                     <a class="nav-link" href="../UI/VenueList.php">Venue</a>
@@ -47,10 +47,6 @@ session_start();
                                     </div>
                                 </li>
                             </ul>
-                            <form class="form-inline my-2 my-lg-0">
-                                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                            </form>
                         </div>
                     </nav>
                 </header>
@@ -70,7 +66,7 @@ session_start();
                                     <a class="nav-link" href="../UI/HomePageEvents.php">Events</a>
                                 </li>
                                 <li class="nav-item active">
-                                    <a class="nav-link" href="../UI/Announcement.php">Announcement</a>
+                                    <a class="nav-link" href="../UI/CreateAnnouncement.php">Announcement</a>
                                 </li>
                                 <li class="nav-item active">
                                     <a class="nav-link" href="../UI/VenueList.php">Venue</a>
@@ -85,10 +81,6 @@ session_start();
                                     </div>
                                 </li>
                             </ul>
-                            <form class="form-inline my-2 my-lg-0">
-                                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                            </form>
                         </div>
                     </nav>
                 </header>
@@ -101,7 +93,6 @@ session_start();
                         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>
-
                         <div class="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul class="navbar-nav mr-auto">
                                 <li class="nav-item active">
@@ -117,10 +108,6 @@ session_start();
                                     </div>
                                 </li>
                             </ul>
-                            <form class="form-inline my-2 my-lg-0">
-                                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                            </form>
                         </div>
                     </nav>
                 </header>                
@@ -180,10 +167,6 @@ session_start();
                                     </div>
                                 </li>
                             </ul>
-                            <form class="form-inline my-2 my-lg-0">
-                                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                            </form>
                         </div>
                     </nav>
                 </header>
