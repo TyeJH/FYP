060ddd045d14552704f4b7dcad6fcd6c50a75aea
USER <USER@LAPTOP-RNH6V26M>
2020-10-29 23:52:49 +0800

    Wait for combine and do some tidy up

--- UI/CreateAnnouncement.php
+++ UI/CreateAnnouncement.php
@@ -12,7 +12,7 @@ session_start();
 
 <html>
     <head>  
-        <title>Venue Page</title>
+        <title>Announcement Page</title>
         <!--        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
                 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
                 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
@@ -22,6 +22,8 @@ session_start();
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
     </head>
     <body>
+        
+        
         <div id="bigcontainer">
             <br><br> 
             <a href="../UI/HomePage.php">Back</a>
@@ -231,5 +233,6 @@ session_start();
                 });
             });
         </script>
+        
     </body>
 </html>
