4b1fc0c573a9535f3cb9e22f4e225ee1d9bf8ce8
kianhee <n.kianhee99@gmail.com>
2020-11-02 01:25:30 +0800

    kianhee updates

--- UI/header.php
+++ UI/header.php
@@ -1,10 +1,10 @@
 <html>
     <head>
         <meta charset="UTF-8">
-<!--        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">-->
+        <!--        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">-->
         <link rel="stylesheet" href="../css/headerStackpath.css">
-        
-<!--        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />-->
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
@@ -58,7 +58,7 @@
                         <div class="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul class="navbar-nav mr-auto">
                                 <li class="nav-item active">
-                                    <a class="nav-link" href="../UI/HomePageEvents.php">Events</a>
+                                    <a class="nav-link" href="../UI/EventOrganizerHome.php">Event Management</a>
                                 </li>
                                 <li class="nav-item active">
                                     <a class="nav-link" href="../UI/CreateAnnouncement.php">Announcement</a>
@@ -72,6 +72,7 @@
                                     </a>
                                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                                         <a class="dropdown-item" href="../UI/UserProfile.php">View Profile</a>
+                                        <a class="dropdown-item" href="ChangePassword.php">Change Password</a>
                                         <a class="dropdown-item" href="../UI/Logout.php">Logout</a>
                                     </div>
                                 </li>
